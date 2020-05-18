7a8dc90e927094f00d69167b42735e4013397179
Radomir <Ja@DESKTOP-RU4GA6R>
2020-05-19 01:10:49 +0200

    Final commit

--- highchart-test-project/src/main/java/io/radomir/HighchartTestProjectApplication.java
+++ highchart-test-project/src/main/java/io/radomir/HighchartTestProjectApplication.java
@@ -1,7 +1,5 @@
 package io.radomir;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
